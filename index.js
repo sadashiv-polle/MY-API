2a6d654a6cf2da4f9429e0195992e521e190a435
Sadashiv Polle <sadashivpolle@MacBookAir.lan>
2025-07-13 14:14:48 +0530

    Added Title

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.get("/", (req, res) => {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Welcome to My API</title>
+ <title>Daily Dose of Inspiration</title>
   <style>
     * { margin: 0; padding: 0; box-sizing: border-box; }
     body {
